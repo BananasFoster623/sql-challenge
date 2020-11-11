feb85b1e497ba0cffdccfa4d7b58b7feb06b524c
bananasfoster623 <61858678+bananasfoster623@users.noreply.github.com>
2020-11-10 20:29:23 -0500

    Completed queries 2, 5, and 8.

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -4,7 +4,8 @@ INNER JOIN salaries ON employees.emp_no = salaries.emp_no
 ORDER BY emp_no ASC
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-
+SELECT first_name, last_name, hire_date FROM employees WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986'
+ORDER BY hire_date ASC
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
@@ -13,7 +14,9 @@ ORDER BY emp_no ASC
 
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+SELECT first_name, last_name, sex FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+ORDER BY last_name ASC
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
@@ -22,3 +25,6 @@ ORDER BY emp_no ASC
 
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(last_name) FROM employees
+GROUP BY last_name
+ORDER BY count DESC
