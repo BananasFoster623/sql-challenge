26d44ce6a0ca5629ce8e29c88fdaf59fd18e8be7
bananasfoster623 <61858678+bananasfoster623@users.noreply.github.com>
2020-11-10 20:48:14 -0500

    Completed all the queries.

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -14,7 +14,10 @@ INNER JOIN employees AS emp ON emp.emp_no = dptmgr.emp_no
 ORDER BY dept_no ASC
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT emp.emp_no, emp.last_name, emp.first_name, dpt.dept_name FROM employees AS emp
+INNER JOIN dept_emp ON dept_emp.emp_no = emp.emp_no
+INNER JOIN departments AS dpt ON dept_emp.dept_no = dpt.dept_no
+ORDER BY dept_name ASC
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex FROM employees
@@ -22,10 +25,18 @@ WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
 ORDER BY last_name ASC
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+SELECT emp.emp_no, emp.last_name, emp.first_name, dpt.dept_name FROM employees AS emp
+INNER JOIN dept_emp ON dept_emp.emp_no = emp.emp_no
+INNER JOIN departments AS dpt ON dept_emp.dept_no = dpt.dept_no
+WHERE dept_name = 'Sales'
+ORDER BY dept_name ASC
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT emp.emp_no, emp.last_name, emp.first_name, dpt.dept_name FROM employees AS emp
+INNER JOIN dept_emp ON dept_emp.emp_no = emp.emp_no
+INNER JOIN departments AS dpt ON dept_emp.dept_no = dpt.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development'
+ORDER BY dept_name ASC
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name, count(last_name) FROM employees
